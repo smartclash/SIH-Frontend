91a0f1c2286edce4bf9c97b7dab96b862ef82784
Karan Sanjeev <yo@alphaman.me>
2022-08-19 22:06:48 +0530

    Fix nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
 }
 
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = nextConfig
