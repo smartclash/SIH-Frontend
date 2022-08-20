1db992f3643aa13f1b6ef92195949e896cf1e693
Karan Sanjeev <yo@alphaman.me>
2022-08-21 00:39:16 +0530

    Redesign blog page

--- pages/blogs/create.tsx
+++ pages/blogs/create.tsx
@@ -1,16 +1,39 @@
 import type {NextPage} from 'next'
 
-import CreateBlog from '@/components/Blogs/CreateBlogs'
-
 const BlogPage: NextPage = () => (
     <section className='hero has-background-white-bis is-fullheight-with-navbar'>
         <div className='hero-body'>
-
-            <div className='container box'>
-                <div className='mb-6'>
-                    <p className='title is-1'>Add Blog</p>
+            <div className='container'>
+                <div className='columns'>
+                    <div className='column is-half is-offset-3'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <p className='card-header-title'>Create Blog</p>
+                            </div>
+                            <div className='card-content'>
+                                <form action=''>
+                                    <div className='field'>
+                                        <label htmlFor='title' className='label'>Title</label>
+                                        <div className='control'>
+                                            <input type='text' className='input' id='title' />
+                                        </div>
+                                    </div>
+                                    <div className='field'>
+                                        <label htmlFor='message' className='label'>Message</label>
+                                        <div className='control'>
+                                            <textarea className='textarea' id='message' style={{minHeight: '20em'}}></textarea>
+                                        </div>
+                                    </div>
+                                    <div className='field'>
+                                        <div className='control'>
+                                            <input type='submit' value='Submit' className='button is-primary is-outlined is-fullwidth' />
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <CreateBlog />
             </div>
         </div>
     </section>
