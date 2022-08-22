be9517893f31c7ccf735c4590d61f47656fdcc5e
Adhav712 <adhav712@gmail.com>
2022-08-22 17:16:15 +0530

    Redesign Notification create Page

--- components/Notifications/CreateNotifications.tsx
+++ components/Notifications/CreateNotifications.tsx
@@ -9,52 +9,58 @@ const CreateNotifications: NextPage = () => {
 
     return (
         <>
-            <label className='label'>Title</label>
-            <input className='input is-link mb-5' type='text' placeholder='What you want them to see...' />
-
-            <div className='field mb-5'>
-                <label className='label'>Notification Spread Level</label>
-                <div className='control'>
-                    <div className='select'>
-                        <select onChange={event => {
-                            console.log(event.target.value)
-                            if (event.target.value === 'L1') {
-                                setL1Selected(false)
-                                console.log(L1Selected)
-                            } else {
-                                setL1Selected(true)
-                            }
-                        }}>
-                            <option value='L3'>L3 - Just sends notification</option>
-                            <option value='L2'>L2 - Sends email and SMS</option>
-                            <option value='L1'>L1 - Makes a voicecall and plays the message</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            {
-                (L1Selected) ? (
+            <div className='card-content'>
+                <form action=''>
                     <div className='field'>
-                        <label className='label'>Message</label>
+                        <label htmlFor='title' className='label'>Title</label>
                         <div className='control'>
-                            <textarea className='textarea' placeholder='Express your thought' style={{minHeight: '15em'}}></textarea>
+                            <input type='text' className='input' id='title' />
                         </div>
                     </div>
-                ) : (
-                    <div className='field mb-5'>
-                        <label className='label'>⚠️ This notification make a emergency call to everyone ⚠️</label>
+                    <div className='field'>
+                        <div className='field mb-5'>
+                            <label className='label'>Notification Spread Level</label>
+                            <div className='control'>
+                                <div className='select is-fullwidth'>
+                                    <select onChange={event => {
+                                        console.log(event.target.value)
+                                        if (event.target.value === 'L1') {
+                                            setL1Selected(false)
+                                            console.log(L1Selected)
+                                        } else {
+                                            setL1Selected(true)
+                                        }
+                                    }}>
+                                        <option value='L3'>L3 - Just sends notification</option>
+                                        <option value='L2'>L2 - Sends email and SMS</option>
+                                        <option value='L1'>L1 - Makes a voicecall and plays the message</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        {
+                            (L1Selected) ? (
+                                <div className='field'>
+                                    <label className='label'>Message</label>
+                                    <div className='control'>
+                                        <textarea className='textarea' placeholder='Express your thought' style={{minHeight: '15em'}}></textarea>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className='field mb-5'>
+                                    <label className='label'>⚠️ This notification make a emergency call to everyone ⚠️</label>
+                                </div>
+
+                            )
+
+                        }
                     </div>
-
-                )
-
-            }
-            <div className='field is-grouped'>
-                <div className='control'>
-                    <button className='button is-link'>Submit</button>
-                </div>
-                <div className='control'>
-                    <button className='button is-link is-light'>Cancel</button>
-                </div>
+                    <div className='field'>
+                        <div className='control'>
+                            <input type='submit' value='Submit' className='button is-primary is-outlined is-fullwidth' />
+                        </div>
+                    </div>
+                </form>
             </div>
         </>
     )
--- pages/notifications/create.tsx
+++ pages/notifications/create.tsx
@@ -5,11 +5,17 @@ import Create from '@/components/Notifications/CreateNotifications'
 const NotificationsPage: NextPage = () => (
     <section className='hero has-background-white-bis is-fullheight-with-navbar'>
         <div className='hero-body'>
-            <div className='container box'>
-                <div className='mb-6'>
-                    <p className='title is-1'>Create Notifications</p>
+            <div className='container'>
+                <div className='columns'>
+                    <div className='column is-half is-offset-3'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <p className='card-header-title'>Create Notifications</p>
+                            </div>
+                            <Create />
+                        </div>
+                    </div>
                 </div>
-                <Create />
             </div>
         </div>
     </section>
