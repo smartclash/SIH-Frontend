ec09bc815d74433a7238da1c72a90969c0edfcfe
Karan Sanjeev <yo@alphaman.me>
2022-08-25 14:44:06 +0530

    Remove custom styles

--- pages/notifications/view.tsx
+++ pages/notifications/view.tsx
@@ -1,31 +1,27 @@
 import type {NextPage} from 'next'
 import Link from 'next/link'
-import styles from '../../styles/Notifications.module.scss'
-// Import ViewNotificationCard from '@/components/Notifications/ViewCard'
 import NotificationInterface from '@/interfaces/Notifications/NotificationInterface'
 
 const ViewNotificationCard: NextPage<NotificationInterface> = props => (
     <>
         <div className='card'>
             <header className='card-header is-flex is-justify-content-space-between'>
-
                 <p className='card-header-title is-size-5'>
-                    { props.Title }
+                    {props.Title}
                 </p>
                 <Link href={''}>
                     <p className='card-header-title is-flex is-justify-content-flex-end'>
-                        @{ props.Author }
+                        @{props.Author}
                     </p>
                 </Link>
             </header>
 
-            <div className='card-content' style={{padding: '1rem'}}>
-                <div className='content '>
-                    {(props.Message)}
+            <div className='card-content'>
+                <div className='content'>
+                    {props.Message}
                     <br />
                 </div>
             </div>
-
         </div>
         <div className='is-flex is-flex-direction-row-reverse mr-4'>
             <h1 className='is-size-7'>{props.TimeStamp} seconds ago</h1>
