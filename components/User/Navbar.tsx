395670bf8dd015e5f0b6fe44b77295a283398be0
kiridharan <kiridharan217dec2001@gmail.com>
2022-08-25 11:26:57 +0530

    appname

--- components/User/Navbar.tsx
+++ components/User/Navbar.tsx
@@ -1,10 +1,11 @@
 import type {NextPage} from 'next'
 
 const Navbar: NextPage = () => (
+    
     <nav className='navbar'>
         <div className='container'>
             <div className='navbar-brand'>
-                <a href='#' className='navbar-item is-size-4'>SIH App</a>
+                <a href='#' className='navbar-item is-size-4'>Rakshak</a>
             </div>
             <div className='navbar-menu'>
                 <div className='navbar-end'>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 import Navbar from '@/components/User/Navbar'
 import Footer from '@/components/User/Footer'
 import '../styles/bulma.sass'
+import Image from 'next/image'
 
 // @TODO: Should use layouts
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
+            <Head>
+                <title>Rakshak</title>
+            </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
