fbe02704f9e741ad8648d046fecbad12f738561e
Karan Sanjeev <yo@alphaman.me>
2022-08-25 16:52:38 +0530

    Auth hook, untested

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    baseURL: 'http://localhost:8000/api/',
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
