edba36926dfec8682db8d9e7e06f6e1d77395dea
Adhav712 <adhav712@gmail.com>
2022-08-25 11:13:27 +0530

    Section tag added

--- pages/institution/[instituteid].tsx
+++ pages/institution/[instituteid].tsx
@@ -1,6 +1,5 @@
 import {NextPage} from 'next'
 import Link from 'next/link'
-import {useState} from 'react'
 import FormInstitutite from '@/interfaces/Institution/Form'
 
 const ViewCard: NextPage<FormInstitutite> = props => (
@@ -41,79 +40,81 @@ const ViewInstitution: NextPage = () => {
     const isNdrfAdmin = true
     return (
         <>
-            <section className='hero has-background-white-bis is-fullheight-with-navbar'>
-                <div className='column is-half is-offset-3'>
-                    <div className='mb-6'>
-                        <p className='is-size-2 has-text-centered-desktop'><b>Institutions details</b></p>
-                    </div>
+            <section className = 'section'>
+                <section className='hero has-background-white-bis is-fullheight-with-navbar'>
+                    <div className='column is-half is-offset-3'>
+                        <div className='mb-6'>
+                            <p className='is-size-2 has-text-centered-desktop'><b>Institutions details</b></p>
+                        </div>
 
-                    {InstitutionView.map((tile, index) => (
-                        <ViewCard {...tile} key={index} />
-                    ))}
-                    {
-                        (isNdrfAdmin) ? (
-                            <div className='card'>
-                                <div className='card-header'>
-                                    <p className='card-header-title'>Add Admin</p>
+                        {InstitutionView.map((tile, index) => (
+                            <ViewCard {...tile} key={index} />
+                        ))}
+                        {
+                            (isNdrfAdmin) ? (
+                                <div className='card'>
+                                    <div className='card-header'>
+                                        <p className='card-header-title'>Add Admin</p>
+                                    </div>
+                                    <div className='card-content'>
+                                        <form action=''>
+                                            <div className='field'>
+                                                <label htmlFor='title' className='label'>Name</label>
+                                                <div className='control'>
+                                                    <input type='text' className='input' id='title' />
+                                                </div>
+                                            </div>
+                                            <div className='field'>
+                                                <label htmlFor='title' className='label'>Email</label>
+                                                <div className='control'>
+                                                    <input type='text' className='input' id='title' />
+                                                </div>
+                                            </div>
+                                            <div className='field'>
+                                                <label htmlFor='title' className='label'>Phone</label>
+                                                <div className='control'>
+                                                    <input type='text' className='input' id='title' />
+                                                </div>
+                                            </div>
+                                            <div className='field'>
+                                                <label htmlFor='title' className='label'>Password</label>
+                                                <div className='control'>
+                                                    <input type='text' className='input' id='title' />
+                                                </div>
+                                            </div>
+                                            <div className='field'>
+                                                <div className='control pt-2'>
+                                                    <input type='submit' value='Submit' className='button is-primary is-outlined is-fullwidth' />
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div className='card-content'>
-                                    <form action=''>
-                                        <div className='field'>
-                                            <label htmlFor='title' className='label'>Name</label>
-                                            <div className='control'>
-                                                <input type='text' className='input' id='title' />
-                                            </div>
-                                        </div>
-                                        <div className='field'>
-                                            <label htmlFor='title' className='label'>Email</label>
-                                            <div className='control'>
-                                                <input type='text' className='input' id='title' />
-                                            </div>
-                                        </div>
-                                        <div className='field'>
-                                            <label htmlFor='title' className='label'>Phone</label>
-                                            <div className='control'>
-                                                <input type='text' className='input' id='title' />
-                                            </div>
-                                        </div>
-                                        <div className='field'>
-                                            <label htmlFor='title' className='label'>Password</label>
-                                            <div className='control'>
-                                                <input type='text' className='input' id='title' />
-                                            </div>
-                                        </div>
-                                        <div className='field'>
-                                            <div className='control pt-2'>
-                                                <input type='submit' value='Submit' className='button is-primary is-outlined is-fullwidth' />
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        ) : ''}
-                    <br></br>
+                            ) : ''}
+                        <br></br>
 
-                    <div className='card'>
-                        <div className='card-content'>
-                            <div className='columns is-half'>
-                                <div className='column'>
-                                    <Link href={'/'}>
-                                        <a className='button is-primary is-outlined is-fullwidth'>
-                                            View Admin
-                                        </a>
-                                    </Link>
-                                </div>
-                                <div className='column'>
-                                    <Link href={'/'}>
-                                        <a className='button is-fullwidth'>
-                                            View Volunteers
-                                        </a>
-                                    </Link>
+                        <div className='card'>
+                            <div className='card-content'>
+                                <div className='columns is-half'>
+                                    <div className='column'>
+                                        <Link href={'/'}>
+                                            <a className='button is-primary is-outlined is-fullwidth'>
+                                                View Admin
+                                            </a>
+                                        </Link>
+                                    </div>
+                                    <div className='column'>
+                                        <Link href={'/'}>
+                                            <a className='button is-fullwidth'>
+                                                View Volunteers
+                                            </a>
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </section>
             </section>
 
         </>
